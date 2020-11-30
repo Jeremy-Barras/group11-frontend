c6f7c8a33e5b7e938142cd49c098046b66380b2b
pierre.debin@student.vinci.be <&1XLViXm>
2020-11-30 15:07:50 +0100

    add - clicable logo + sound image

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 /* load bootstrap module (JS) */
 import 'bootstrap';
 
-const HEADER_PICTURE = " <img id='headerLogo' src= " + logo +"  >";
+const HEADER_PICTURE = "<a href='/'><img id='headerLogo' src= " + logo +"  ></a>";
 const PAGE_TITLE = "Demo : use of webpack as a module bundler";
 const FOOTER_SUPPORT_TEXT = "Support contact : jeremy.barras@student.vinci.be";
 const FOOTER_CREATE_BY_TEXT = "Create by : Group 11 of IPL Student";
@@ -18,3 +18,4 @@ const FOOTER_CREATE_BY_TEXT = "Create by : Group 11 of IPL Student";
 Router();
 
 setLayout(HEADER_PICTURE, PAGE_TITLE, FOOTER_SUPPORT_TEXT, FOOTER_CREATE_BY_TEXT);
+
